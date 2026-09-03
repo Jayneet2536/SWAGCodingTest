dd9e8edd74a5aa6a536dbcd16565eaea40e9bdf4
Jayneet2536 <jayneet597@gmail.com>
2026-09-04 01:41:47 +0530

    initialize firebase app and export auth and firestore instances

--- codingPlatform/firebase.js
+++ codingPlatform/firebase.js
@@ -1,18 +1,25 @@
-// firebase.js
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC1OLyOdDyt3KEr7acgayAESBQhiiebIbk",
-  authDomain: "club-quiz.firebaseapp.com",
-  projectId: "club-quiz",
-  storageBucket: "club-quiz.firebasestorage.app",
-  messagingSenderId: "503052613912",
-  appId: "1:503052613912:web:3d39800cfaf81b2b5c866d",
-  measurementId: "G-SNPGVCSGX6"
+  apiKey: "AIzaSyCxXTczUBLcd2kr1SeLEV_BqsW0a7aW1Xc",
+  authDomain: "swagtest-497e2.firebaseapp.com",
+  projectId: "swagtest-497e2",
+  storageBucket: "swagtest-497e2.firebasestorage.app",
+  messagingSenderId: "511902241169",
+  appId: "1:511902241169:web:1b13bef7b0a77231bd8a30",
+  measurementId: "G-8LMSD9K5QV"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
